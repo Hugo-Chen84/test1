17b013bf79268f9155c14ec3be0f8f2a241ef4a3
Hugo <3573656509@qq.com>
2025-08-11 08:20:13 +0800

    Add files via upload

---
+++ project2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+short main()
+ {
+	printf(" % d\n", sizeof(short));
+	printf(" % d\n", sizeof(long)); 
+	printf(" % d\n", sizeof(char));
+	printf(" % d\n", sizeof(int)); 
+	printf(" % d\n", sizeof(long long));
+	getchar();
+	return 0;
+}
