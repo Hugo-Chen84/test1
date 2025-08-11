631eb646fee3b76ce34389a16a09eb0a2c469b74
Hugo <3573656509@qq.com>
2025-08-11 09:10:44 +0800

    Update and rename 源 (8).c to 使用while循环.c

---
+++ 使用while循环.c
@@ -0,0 +1,19 @@
+#include <stdio.h>//Ñ­»·Óï¾ä 
+int main()
+{
+	int line = 0;
+	while (line < 99)
+	{
+		printf("Ð´´úÂë:%d\n",line);
+		line++;
+	}
+	if (line >= 100)
+	{
+		printf("ºÃoffer\n");
+	}
+	else
+	{
+		printf("¼ÌÐø¼ÓÓÍ\n");
+	}
+	return 0;
+}
