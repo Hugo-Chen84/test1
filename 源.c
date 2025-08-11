ebb6f65ed6995058b72d6f1ec77c997b98b3ccae
Hugo <3573656509@qq.com>
2025-08-11 08:25:58 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,16 @@
+#include <stdio.h>,<string.h>
+int main()
+{
+	char arr1[] = "abcedf";
+	char arr2[] = { 'a','b','c','d','e','f' };
+	printf(" % s\n", arr1);
+	printf(" % s\n", arr2);//按fn+f10,监视可发现不同。
+	char arr3[] = { 'a','b','c','d','e','f','\0' };
+	printf(" % s\n", arr3);
+	int len1 = strlen(arr1);
+	int len2 = strlen(arr2);
+	int len3 = strlen(arr3);
+	printf("%d\n %d\n %d\n", len1, len2, len3);
+	printf("\a\a\a\a");
+	return 0;
+}
