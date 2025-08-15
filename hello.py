757356d25a849c890f5f9b628454983a6d212c6a
Hugo <3573656509@qq.com>
2025-08-15 18:00:17 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+import math
+print ("Hello,Python")
+print ("Hello"+' Python')
+print ('haha'*2)
+print ('''三引号换行好用
+黄河之水天上来，
+奔流到海不复回''')
+result1=math.gcd(8,12)
+print (result1)
+result2=math.sin(1)
+print (result1,result2)
